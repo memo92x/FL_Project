7b095400a2ef434840ad42c44f73d563f3e654e8
mrsonay <sonay2@gmail.com>
2015-09-21 20:25:10 +0200

    Dette er en test

---
+++ FL_Project/Controllers/FreelancerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FL_Project.Controllers
+{
+    public class FreelancerController : Controller
+    {
+        //
+        // GET: /Freelancer/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+         
+        public ActionResult Preview()
+        {
+            return View();
+        }
+    }
+}
---
+++ FL_Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FL_Project.MvcApplication" Language="C#" %>
